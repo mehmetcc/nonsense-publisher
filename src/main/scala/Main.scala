7d1d8dca624d42fc6cf14ff95ac89cab567979a5
mehmetcc <mehmetccm@gmail.com>
2025-01-20 14:59:19 +0300

    Fix Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,8 @@
-import User.encoder
 import zio._
-import zio.json.EncoderOps
 
 object Main extends ZIOAppDefault {
   override def run: ZIO[Environment with ZIOAppArgs with Scope, Any, Any] =
     program.provide(Configuration.live, PublisherImpl.live)
 
-  private val program =
-    for {
-      encoded <- ZIO.succeed(User.next.toJson)
-      _       <- Runner.forever
-    } yield ()
+  private val program = Runner.forever
 }
