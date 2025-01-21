6ef11cf452028d21b661a5ee985f66e78d9b4322
mehmetcc <mehmetccm@gmail.com>
2025-01-21 09:16:29 +0300

    Remove redundant line

--- src/main/scala/Runner.scala
+++ src/main/scala/Runner.scala
@@ -5,7 +5,6 @@ import zio.{RIO, ZIO}
 object Runner {
   val forever: RIO[Publisher with Configuration, Unit] = for {
     configuration <- ZIO.service[Configuration]
-    entity         = User.next.toJson
     _             <- ZIO.foreachParDiscard(1 to configuration.parallelism)(_ => effect.forever)
   } yield ()
 
